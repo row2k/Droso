8900ab52ece0bdebe69dd1d46011f7b8b2829b6f
brittwalker <brittanywalker@Brittanys-MBP.fios-router.home>
2017-01-03 15:48:05 -0500

    fixed the weird smooth scroll dynamic bug

--- js/main.js
+++ js/main.js
@@ -7,10 +7,19 @@ $(document).ready(function() {
       }
     });
 
-    $('a').click(function(){
+    $(document).on('click', 'a', function(event){
+        event.preventDefault();
+
         $('html, body').animate({
-            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 50
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 50
         }, 500);
-        return false;
     });
+
+    // $('a').click(function(){
+    //     $('html, body').animate({
+    //         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 50
+    //     }, 500);
+    //     return false;
+    // });
+
 });
