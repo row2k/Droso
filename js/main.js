14bbd362eb451d7439e4b27b9186de86c8ad3a21
brittwalker <brittanywalker@Brittanys-MBP.fios-router.home>
2017-01-03 16:38:15 -0500

    some styling updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+      if ($(document).scrollTop() > 0) { // check if user scrolled more than 50 from top of the browser window
         $(".navbar").addClass("navcolor"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
       } else {
         $(".navbar").removeClass("navcolor"); // if not, change it back to transparent
@@ -11,15 +11,11 @@ $(document).ready(function() {
         event.preventDefault();
 
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 50
-        }, 500);
-    });
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 75
+        }, 500).delay(200);
 
-    // $('a').click(function(){
-    //     $('html, body').animate({
-    //         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 50
-    //     }, 500);
-    //     return false;
-    // });
+        $("a").css("border","2px dashed rgba(0,0,0,0)");
+        $(this).css("border","2px dashed white");
+    });
 
 });
